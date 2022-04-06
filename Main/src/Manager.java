2e76a070b001d33285c651308d653fe6ae28af0b
mariacorona <mfcoronaortega@miners.utep.edu>
2022-04-06 11:47:38 -0600

    Account for object Manager for reading and writing, text file with some sample data Test run for executables

---
+++ Main/src/Manager.java
@@ -0,0 +1,99 @@
+import javax.lang.model.type.NullType;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+
+
+public class Manager{
+    public static String file_name = "/Users/mariacorona/IdeaProjects/Term-Project-CS4387/Main/src/Mock_BankDB.txt";
+    public static int number_of_accouts = fileLines();
+    public static Account [] myaccounts =  new Account[number_of_accouts];
+
+    public static int fileLines(){ //Helper method to get the number of accounts accurately
+        Path path = Paths.get(file_name);
+
+        long lines = 0;
+        try {
+
+            // much slower, this task better with sequence access
+            //lines = Files.lines(path).parallel().count();
+
+            lines = Files.lines(path).count();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return (int)lines;
+    }
+
+    public static Account [] readData(){ //Reads a text file with account information and turns it into an array of accounts
+        String [] myData =  new String[5];
+        try {
+            File myObj = new File(file_name);
+            Scanner myReader = new Scanner(myObj);
+            int x = 0;
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                myData = data.split(" ");
+                Account myAccount = new Account(myData[0],myData[1],myData[2],Double.parseDouble(myData[3]),myData[4]);
+                myaccounts[x] = myAccount;
+                x++;
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return myaccounts;
+    }
+
+    public static void writeData(Account myAccount){ //writes new account onto file
+        try(FileWriter fw = new FileWriter(file_name, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.print("\n"+myAccount.getUsername() + " ");
+            out.print(myAccount.getPassword()  + " ");
+            out.print(myAccount.getAccount_number() + " ");
+            out.print(myAccount.getAccount_balance()+ " ");
+            out.print(myAccount.getType());
+
+        } catch (IOException e) {
+            //exception handling left as an exercise for the reader
+        }
+    }
+
+    public Account findAccount(String account_number){ //Method to find an account thorugh account number
+        Account newAccount = new Account("Null","Null","Null",0.00,"Null");
+
+        for(int x =0; x < myaccounts.length; x++){
+            if (myaccounts[x].getAccount_number().matches(account_number)){
+                return myaccounts[x];
+            }
+        }
+        System.out.println("Sorry, no account with that account number was found.");
+        return newAccount;
+
+    }
+
+    public static void printAccounts(){ //Test method to print current accounts list
+
+        for(int x =0; x < myaccounts.length; x++){
+            System.out.print(myaccounts[x].getUsername() + " ");
+            System.out.print(myaccounts[x].getAccount_number()+ " ");
+            System.out.print(myaccounts[x].getPassword()+ " ");
+            System.out.print(myaccounts[x].getAccount_balance()+ " ");
+            System.out.println(myaccounts[x].getType());
+        }
+
+    }
+
+}
---
+++ Main/src/testrun.java
@@ -0,0 +1,14 @@
+public class testrun {
+
+    //HEADS UP
+    // the text file can be appended to but must not end in a new line
+
+    public static void main(String[] args) {
+        Manager.readData();
+        Manager.printAccounts();
+        //need to test find account still
+        Account newAccount = new Account("Null","Null","Null",0.00,"Null");
+        Manager.writeData(newAccount);
+    }
+}
+
