54c54173f00223fd669895792f0c09409f500321
amnavarro2 <78327831+amnavarro2@users.noreply.github.com>
2022-04-22 18:47:35 -0600

    added money transfer test here to work

--- Main/src/ManagerTest.java
+++ Main/src/ManagerTest.java
@@ -45,5 +45,56 @@ class ManagerTest {
         Boolean foundUsername3 = Manager.findUsername(username3);
         assertEquals( check, foundUsername3);
     }
+    
+    
+    @Test
+    public void valid_transfer_test(){
+        double money = 10.0;
+        money_transfer acc = new money_transfer();
+
+        //Account main_acc = new Account();
+        Account main_acc = Manager.findAccount("12345678");
+        System.out.println(main_acc.getUsername());
+
+
+        //Account sec_acc = Manager.findAccount("00932734");
+
+        //change the transfer money to force input instead of using the scanner
+        assertTrue(money_transfer.transfer_money(main_acc,money, "98663859"));
+    }
+
+    @Test
+    public void invalid_transfer_test(){
+        double money = 10.0;
+        money_transfer acc = new money_transfer();
+
+        //Account main_acc = new Account();
+        Account main_acc = Manager.findAccount("0000000000");
+        System.out.println(main_acc.getUsername());
+
+
+        //Account sec_acc = Manager.findAccount("00932734");
+
+        //change the transfer money to force input instead of using the scanner
+        assertFalse(money_transfer.transfer_money(main_acc,money, "98663859"));
+    }
+    
+     @Test
+    void UsernameNotFound() { //Checks if find username is yielding correctly when username is found
+        Boolean check = true;
+
+        String username1 = "alexis";
+        Boolean foundUsername1 = Manager.findUsername(username1);
+        assertNotEquals( check, foundUsername1);
+
+        String username2 = "alex";
+        Boolean foundUsername2 = Manager.findUsername(username2);
+        assertNotEquals( check, foundUsername2);
+
+        String username3 = "alexander";
+        Boolean foundUsername3 = Manager.findUsername(username3);
+        assertNotEquals( check, foundUsername3);
+    }
+
 
 }
